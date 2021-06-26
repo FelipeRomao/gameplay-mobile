2a0e64440b259fc57df6b631214fa161c27f658f
FelipeRomao <feliperomao.a@gmail.com>
2021-06-26 12:28:36 -0300

    simple adjusts

--- src/components/guildicon/index.tsx
+++ src/components/guildicon/index.tsx
@@ -4,7 +4,6 @@ import { Image } from "react-native";
 import { styles } from "./styles";
 
 export default function GuildIcon() {
-  const uri =
-    "https://www.pngitem.com/pimgs/m/529-5297554_transparent-background-discord-logo-transparent-hd-png-download.png";
+  const uri = "https://upload.wikimedia.org/wikipedia/pt/5/5f/COD_Warzone.jpg";
   return <Image source={{ uri }} style={styles.image} resizeMode="cover" />;
 }
